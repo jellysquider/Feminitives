cf6175e3b8b5225f65d1979f622fff1d381e4579
Gabriella Fabwen <geccentrical@gmail.com>
2017-02-07 18:56:48 -0800

    urlparse renamed urllib.parse

--- feminitives/settings.py
+++ feminitives/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'feminitives.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
