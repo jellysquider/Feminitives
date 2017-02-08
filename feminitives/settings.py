89fa756eb374aa28f6cd4fa93fc898e6c14bb880
Gabriella Fabwen <geccentrical@gmail.com>
2017-02-07 18:53:59 -0800

    urlparse renamed urllib.parse

--- feminitives/settings.py
+++ feminitives/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
-import psycopg2
-import urlparse
+# import psycopg2
+# import urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
